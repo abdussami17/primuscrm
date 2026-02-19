74f71150420e49adfffd04e18f960ab9f45c0665
abdussami17 <s.stechpoint123@gmail.com>
2026-02-19 06:46:55 +0500

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1462,7 +1462,6 @@ function warningAlert(message) {
     <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015"
         integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ=="
         data-cf-beacon='{"token":"3ca157e612a14eccbb30cf6db6691c29"}' crossorigin="anonymous"></script>
- -->
 
 <script>
     // Prevent a single modal's close button from accidentally closing multiple stacked modals.
